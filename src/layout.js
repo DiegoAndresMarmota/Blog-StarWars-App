d08f8cd35abe28f3f85ed1a9e5f5a44aa557f0d9
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-12-16 22:13:07 -0300

    Creación de layout

---
+++ src/layout.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ScrollToTop from "./component/scrollToTop";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Home from "./views/home.jsx";
+import Detail from "./views/detail.jsx";
+import Favorites from "./views/favorites.jsx";
+import injectContext from "./store/appContext";
+
+import { Navbar } from "./component/navbar";
+import { Footer } from "./component/footer";
+
+const Layout = () => {
+  const basename = process.env.BASENAME || "";
+
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/:data_type/:id">
+              <Detail />
+            </Route>
+            <Route exact path="/favorites">
+              <Favorites />
+            </Route>
+            <Route>
+              <h1>Not found!</h1>
+            </Route>
+          </Switch>
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default injectContext(Layout);
