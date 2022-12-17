8d965cf05ae70fad48e4954ee8917a793105f4be
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-12-16 22:11:49 -0300

    Creación de views

---
+++ src/views/favorites.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Card } from "../component/Card.jsx"
+
+const Favorites = () => (
+	<div className="container w-75 text-center mt-2">
+		<h1 className="p-4 text-start">My Favorites</h1>
+		<div className="d-flex">
+			<Card className=" col-sm-12 col-lg-6"/>
+			<Card className=" col-sm-12 col-lg-6"/>
+			<Card className=" col-sm-12 col-lg-6"/>
+		</div>
+		<div className="d-flex">
+			<Card className=" col-sm-12 col-lg-6"/>
+			<Card className=" col-sm-12 col-lg-6"/>
+			<Card className=" col-sm-12 col-lg-6"/>
+		</div>
+		<div className="d-flex">
+			<Card className=" col-sm-12 col-lg-6"/>
+			<Card className=" col-sm-12 col-lg-6"/>
+			<Card className=" col-sm-12 col-lg-6"/>
+		</div>
+	</div>
+);
+
+export default Favorites;
---
+++ src/views/home.jsx
@@ -0,0 +1,25 @@
+import React, { useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
+import { Carousel } from "../component/Carousel.jsx";
+
+const Home = () => {
+
+  const { store, actions } = useContext(Context);
+  const data_types = Object.keys(store.results);
+
+  useEffect(() => {
+    for (let data_type of data_types) {
+      actions.getData(data_type)
+    }
+  }, []);
+
+  return (
+    <div className="text-center mt-3">
+      {data_types.map((data_type) => (
+        <Carousel data_type={data_type} />
+      ))}
+    </div>
+  )
+}
+
+export default Home
